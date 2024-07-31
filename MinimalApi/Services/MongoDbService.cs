c9cfa1ad4a0a19fd8e43144bb3d77c6aff5ab665
Gabriela Rosa <gabii.ramos1309@gmail.com>
2024-07-31 08:36:32 -0300

    Add project files.

---
+++ MinimalApi/Services/MongoDbService.cs
@@ -0,0 +1,45 @@
+﻿using MongoDB.Driver;
+
+namespace MinimalApi.Services
+{
+    public class MongoDbService
+    {
+        /// <summary>
+        /// Armazena config da aplicacao
+        /// </summary>
+        private readonly IConfiguration _configuration;
+
+        /// <summary>
+        /// Armazena uma ref ao MongoDb
+        /// </summary>
+        private readonly IMongoDatabase _database;
+
+        /// <summary>
+        /// Recebe a config da aplicacao como parametro 
+        /// </summary>
+        /// <param name="configuration">objeto configuration</param>
+        public MongoDbService(IConfiguration configuration)
+        {
+            //Atribui a config  recebida em _configuration 
+            _configuration = configuration;
+
+            //Obtem a string de conexao atraves do _configuration
+            var connectionString = _configuration.GetConnectionString("DbConnection");
+
+            //Cria um obj MongoUrl que recebe como parametro a string de conexao
+            var mongoUrl = MongoUrl.Create(connectionString);
+
+            //Cria um client MongoClient para se conectar ao MongoDb
+            var mongoClient = new MongoClient(mongoUrl);
+
+            //Obtem a ref ao bd com o nome especificado na string de conexao
+            _database = mongoClient.GetDatabase(mongoUrl.DatabaseName);
+        }
+
+        /// <summary>
+        /// Propriedade para acessar o banco de dados 
+        /// Retorna a referencia ao bd
+        /// </summary>
+        public IMongoDatabase GetDatabase => _database;
+    }
+}
